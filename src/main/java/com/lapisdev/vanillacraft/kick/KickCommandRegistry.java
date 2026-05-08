b199a6f37f4849aafb978993a7921a3262f2535b
dylancode <opensource@dylancode.dev>
2026-05-08 10:52:10 +0100

    register dc cmd /mckick

--- src/main/java/com/lapisdev/vanillacraft/kick/KickCommandRegistry.java
+++ src/main/java/com/lapisdev/vanillacraft/kick/KickCommandRegistry.java
@@ -3,11 +3,14 @@ package com.lapisdev.vanillacraft.kick;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 
 import static com.lapisdev.vanillacraft.VanillacraftPlugin.plugin;
+import static com.lapisdev.vanillacraft.discord.Discord.jda;
 
 public class KickCommandRegistry {
     public static void register() {
         plugin().getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, (registry) -> {
             KickCmdMc.register(registry.registrar());
         });
+
+        KickCmdDc.register();
     }
 }
